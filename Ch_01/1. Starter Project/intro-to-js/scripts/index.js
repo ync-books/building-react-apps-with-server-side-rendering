081a0b69c9dfafb40579d41029959ad4d96f9043
yongnan <ync@fastmail.com>
2020-11-09 17:33:26 +0800

    my practices

--- Ch_01/1. Starter Project/intro-to-js/scripts/index.js
+++ Ch_01/1. Starter Project/intro-to-js/scripts/index.js
@@ -1,3 +1,5 @@
+//import { Animal } from './classes/module/Animal';
+
 var ResultContainer = document.getElementById("ResultContainer");
 
 /* let vs. var */
@@ -8,7 +10,9 @@ var ResultContainer = document.getElementById("ResultContainer");
 // ResultContainer.innerHTML =  letVariable;
 //ResultContainer.innerHTML = "Setting up the environment!";
 
-/* Rest parameter */
+/*
+ * Rest parameter
+ */
 // function sum(...inputs) {
 //     var result = 0;
 //     for(let i of inputs){
@@ -26,33 +30,35 @@ var ResultContainer = document.getElementById("ResultContainer");
 // }
 //ResultContainer.innerHTML = sum(5, 10, 5, 5);
 
-/* Destructuring and Spread */
+/*
+ * Destructuring and Spread
+ */
 // let fruits = ['Apple', 'Watermelon', 'Grapes'];
 // let [fruit1, fruit2, fruit3] = fruits;
 // ResultContainer.innerHTML = fruit2;  // Watermelon
 
-// skip element
+/* skip element */
 // let [fruit1, , fruit2] = fruits;
 // ResultContainer.innerHTML = fruit2;  // Grapes
 
 // let [fruit1, ...OtherFruits] = fruits;
 // ResultContainer.innerHTML = OtherFruits;  // Watermelon, Grapes
 
-// Objects can be destructured
+/* Objects can be destructured */
 // let Fruits = {Fruit1: 'Apple', Fruit2: 'Watermelon'};
 // let {Fruit1, Fruit2} = Fruits;
 // ResultContainer.innerHTML = Fruit1;  // Apple
 
-// destructuring in functions
-// function sum(a, b, c){
-//     return a+b+c;
-// }
-// let input = [5,9,6];
-// ResultContainer.innerHTML = sum(...input);  //20
+/* destructuring in functions */
+function sum(a, b, c){
+    return a+b+c;
+}
+let input = [5,9,6];
+ResultContainer.innerHTML = sum(...input);  //20
 
-import { Animal } from './modules/Animals.js';
-let dog = new Animal('dog');
-console.log(dog.getType());
+// let dog = new Animal('dog');
+// console.log(dog.getType());
+//ResultContainer.innerHTML = dog.getType()
 
 var header = document.getElementsByTagName("h1");
 var body = document.getElementById("ResultContainer");
@@ -60,23 +66,3 @@ var footer = document.getElementsByClassName("Footer");
 console.log(header);
 console.log(body);
 console.log(footer);
-
-header[0].textContent = "Header Text from JS";
-header[0].setAttribute('isHeader','True');
-header[0].style.border = '2px solid black';
-console.log(header[0]);
-
-try{
-    var fruit = new Fruit();
-}
-catch(e){
-    console.log('ERROR: ' + e.message);
-}
-console.log('rest of the code!');
-
-try{
-    throw new Error('Custom Developer Error!');
-}
-catch(e){
-    console.log('ERROR: ' + e.message);
-}
